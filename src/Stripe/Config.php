8df2dbdb2915ca7c619bdfcde3c1959ee54bd786
Paul Gomez <pauldro952@outlook.com>
2025-10-16 13:44:22 -0500

    REFACTOR: update stripe config
    - extend apiconfig
    - use SimpleArray for allowedPaymentTypes

--- src/Stripe/Config.php
+++ src/Stripe/Config.php
@@ -1,53 +1,43 @@
 <?php namespace Dpay\Stripe;
 // Pauldro Minicli
-use Pauldro\Minicli\v2\Util\Data;
 use Pauldro\Minicli\v2\Util\EnvVarsReader as EnvVars;
-// Lib
+use Pauldro\Minicli\v2\Util\SimpleArray;
+// Dpay
+use Dpay\Abstracts\Api\AbstractApiConfig;
 use Dpay\Data\PaymentMethod\PaymentMethod;
+use Dpay\Stripe\Services\Events\Util\EventType;
 
 /**
- * Config
  * Container for Stripe API Config Data
  * 
- * @property string $secretKey            API Secret Key
- * @property array  $allowedPaymentTypes  Allowed Payment Types (ach,amazonpay), note if blank, then use stripe defaults
- * @property bool   $useSandbox
- * @property bool   $autoConfirmPreauths  Confirm Pre-Auths?
+ * @method static Config instance()
+ * 
+ * @property string       $secretKey            API Secret Key
+ * @property SimpleArray  $allowedPaymentTypes  Allowed Payment Types (ach,amazonpay), note if blank, then use stripe defaults
+ * @property bool         $useSandbox
+ * @property bool         $autoConfirmPreauths  Confirm Pre-Auths?
  */
-class Config extends Data {
-	private static $instance;
+class Config extends AbstractApiConfig {
+	protected static $instance;
 
 /* =============================================================
 	Constructors / Inits
 ============================================================= */
-	public static function instance() : Config
-	{
-		if (empty(self::$instance) === false) {
-			return self::$instance;
-		}
-		self::$instance = new self();
-		self::$instance->init();
-		return self::$instance;
-	}
-	
 	public function __construct() {
+		parent::__construct();
 		$this->secretKey = '';
-		$this->allowedPaymentTypes = [];
+		$this->allowedPaymentTypes = new SimpleArray();
 		$this->useSandbox = false;
 		$this->autoConfirmPreauths = false;
 	}
 
-	/**
-	 * Parse Field Values from $_ENV
-	 * @return bool
-	 */
-	public function init() : bool
+	protected function init() : void
 	{
 		$this->secretKey  = EnvVars::get('STRIPE.API.SECRETKEY');
 		$this->useSandbox = EnvVars::getBool('STRIPE.API.USESANDBOX');
 		$this->autoConfirmPreauths = EnvVars::getBool('STRIPE.API.PREAUTHS.AUTOCONFIRM');
 		$this->setAllowedPaymentTypesFromEnv();
-		return true;
+		$this->setActionableEventsFromEnv();
 	}
 	
 /* =============================================================
@@ -57,22 +47,36 @@ class Config extends Data {
 	 * Parse Payment Types from $_ENV
 	 * @return bool
 	 */
-	public function setAllowedPaymentTypesFromEnv() : bool 
+	private function setAllowedPaymentTypesFromEnv() : void
 	{
 		if (EnvVars::exists('STRIPE.ALLOWED.PAYMENT.TYPES') === false) {
 			$this->allowedPaymentTypes = [];
-			return true;
+			return;
 		}
 		$types = EnvVars::getArray('STRIPE.ALLOWED.PAYMENT.TYPES');
-		$paymentTypes = [];
 
 		foreach ($types as $type) {
 			if (array_key_exists($type, PaymentMethod::TYPES) === false) {
 				continue;	
 			}
-			$paymentTypes[] = $type;
+			$this->allowedPaymentTypes->set($type, $type);
 		}
-		$this->allowedPaymentTypes = $paymentTypes;
-		return true;
+	}
+
+	private function setActionableEventsFromEnv() : void
+	{
+		if (EnvVars::exists('STRIPE.EVENTS.ACTIONABLE') === false) {
+			return;
+		}
+		$types = EnvVars::getArray('STRIPE.EVENTS.ACTIONABLE');
+
+		foreach ($types as $type) {
+			$eventType = EventType::find($type);
+
+			if ($eventType === false) {
+				continue;
+			}
+			$this->actionableEvents->set($type, $eventType);
+		}
 	}
 }
